596b6b8d7c8e978679d76579c1544aac4423c884
2109231Frank <2109231@students.wits.ac.za>
2022-05-23 14:01:12 +0200

    adding links to repos

--- src/Pages/Work.js
+++ src/Pages/Work.js
@@ -10,16 +10,23 @@ export default class Wireframe extends Component {
             <h1>my work</h1>
             <article>
             <p>
-                This page contains a list of links to previous websites that I have made. Please note that I have made these in university as assignments, 
-                thus there are theory blogposts in the websites since they were assignment requirements. My React site is subject to change since 
-                I am still editing the website for varsity.
+                This page contains a list of links to previous websites that I have made as well as to their 
+                respective github Repositories. Please note that I have made these in university as assignments, 
+                thus there are theory blogposts in the websites since they were assignment requirements. My 
+                React site is subject to change since I am still editing the website for varsity. The Repository 
+                for this site is <a href = "https://github.com/2109231Frank/Portfolio" target="_blank">HERE</a>.
             </p>
             <h2>See My Progress!</h2>
             </article>
-            <ul>
+            <ul className='sites'>
                 <li> <a href = "https://2109231frank.github.io/WSOA3028A_2109231/index.html" target="_blank">2021 first site</a></li>
                 <li> <a href = "https://2109231frank.github.io/WSOA3029A_2109231/index.html" target="_blank">2021 final exam</a></li>
                 <li> <a href = "https://2109231frank.github.io/WSOA4175A_2109231/#/" target="_blank">2022 React site</a></li>
+            </ul>
+            <ul className='repos'>
+                <li> <a href = "https://github.com/2109231Frank/WSOA3028A_2109231" target="_blank">code</a></li>
+                <li> <a href = "https://github.com/2109231Frank/WSOA3029A_2109231" target="_blank">code</a></li>
+                <li> <a href = "https://github.com/2109231Frank/WSOA4175A_2109231" target="_blank">code</a></li>
             </ul>
         
            
