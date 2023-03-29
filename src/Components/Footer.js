ea195e89c42ac30a698858b42838a22c54055d43
Franco Steyn <francohsteyn@gmail.com>
2023-03-29 14:51:39 +0200

    fixed links and footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,6 +3,7 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import "../Style/Footer.css";
 
 let instaLink = "https://www.instagram.com/francohayes_photography/";
+let linkedLink = "https://www.linkedin.com/in/franco-steyn-b4b386188/";
 
 export default class Footer extends Component {
   render() {
@@ -12,7 +13,8 @@ export default class Footer extends Component {
                 <section class = "p-name">&copy; 2022 Franco Steyn</section>
                 <a classname = "insta" href = {instaLink} target="_blank"><InstagramIcon/></a>
                 <section class = "p-postal-code">Area Code - 2194</section>
-                <section class = "email">E-mail me - francohsteyn@gmail.com</section>
+                <section class = "LinkedIn">Please contact me via -<a classname = "LinkedIn" href = {linkedLink} target="_blank">LinkedIn</a></section>
+                <section class = "email">Email me - francohsteyn@gmail.com</section>
             </p>
       </div>
     )
